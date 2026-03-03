66dac35ed32a9aa610dc82c706e4d748f9ef4c2d
hamperlord <penelopeaprilvesenant@gmail.com>
2026-03-02 22:06:25 -0600

    I'm eating glass

--- Fixawin/fixawin.cpp
+++ Fixawin/fixawin.cpp
@@ -1,13 +1,31 @@
+// Mon Mar 2 09:16:10 PM CST 2026
+#include <stdlib.h>
 #include <xcb/xcb.h>
+#include <xcb/xcb_cursor.h>
 #include <xcb/xproto.h>
 
-void setup();
-void setupMouse();
-void handleMouse();
-void handleKeyboard();
-void mapWindow();
+// MACROS
 
+// ENUMS
+
+// FUNCTION DECLARATIONS
+void setup();
+
+// VARIABLES
+xcb_connection_t *dpy;
+xcb_screen_t *scr;
+
+static int running = 1;
+
+// MAIN
 int main(int argc, char *argv[]) {
-  // block formatting
+  setup();
+
   return 0;
+}
+
+// FUNCTIONS
+void setup() {
+  int screen_num;
+  dpy = xcb_connect(NULL, &screen_num);
 }
